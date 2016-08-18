371e6e72ea5b8699b6616904c973beaf5dab7b6a
Jaime Zhong <hokming.zhong@cditsolutions.com>
2016-08-18 15:00:38 -0500

    Added defaultOn option

--- spec/index-spec.js
+++ spec/index-spec.js
@@ -4,7 +4,8 @@ const Hapi = require('hapi'),
 	AuthClient = require('../lib'),
 	Seneca = require('seneca')(),
 	Config = require('../config/config'),
-	request = require('request');
+	request = require('request'),
+	jwt = require('jsonwebtoken');
 
 describe('register()', () => {
 	var server;
@@ -80,33 +81,53 @@ describe('register()', () => {
 	});
 
 	it('will return 200 on successful authentication', (done) => {
-
 		// Authentication Server must be running at port 3002 using TestDao. There's probably a better way to test this while reducing dependencies. Probably a mock of some sort.
 		
 		var client = Seneca.client({
-			type: 'http',
+				type: 'http',
 				port: Config.AuthServer.senecaPort,
 				host: Config.AuthServer.host,
-				pin: Config.AuthServer.pins.login
+				pin: Config.AuthServer.pins.login,
+				protocol: 'https'
 		}).client({
 				type: 'http',
 				port: Config.AuthServer.senecaPort,
 				host: Config.AuthServer.host,
-				pin: Config.AuthServer.pins.validate
+				pin: Config.AuthServer.pins.validate,
+				protocol: 'https'
 		});
 
 		server.register({ register: AuthClient }, (err) => {
 			server.route({
+				method: 'POST',
+				path: '/login',
+				handler: function(request, reply) {
+					client.act({
+						username: request.payload.username,
+						password: request.payload.password,
+						role: 'auth',
+						cmd: 'login'	
+					}, (err, response) => {
+						expect(err).toBeNull();
+						reply(response);
+					});
+				}
+			});
+
+			server.route({
 				method: 'GET',
 				path: '/restricted',
+				config: {
+					auth: 'jwt'
+				},
 				handler: function(request, reply) {
-					var message = Config.AuthServer.pins.validate;
-					message.referenceToken = request.headers.referencetoken;
-					client.act(message, (err, result) => {
-						expect(err).toBeNull();
-						expect(result.identityToken).toBeDefined();
-						reply(result);
-					});
+					if (request.auth.isAuthenticated) {
+						reply({
+							identityToken: request.auth.credentials.identityToken
+						});
+					} else {
+						reply({ success: false });
+					}
 				}
 			});
 		});
@@ -117,26 +138,73 @@ describe('register()', () => {
 
 
 		// TODO: When AuthServer updates login method to require a hashed password in the payload, this test will need to replace the password supplied
-		client.act({
-			username: 'jsmith',
-			password: 'password',
-			role: 'auth',
-			cmd: 'login'
-		}, (err, response) => {
-			expect(err).toBeNull();
-
-			var options = {
-				url: 'http://localhost:3000/restricted',
-				headers: {
-					'referencetoken': response.referenceToken
+		
+		request.post('http://localhost:3000/login'
+			, { 
+				form: {
+					username: 'jsmith',
+					password: 'password'
 				}
-			}
-			request.get(options, (err, response, body) => {
-				expect(response.statusCode).toBe(200);
-				console.log(body);
-				done();
+			}, (err, response, body) => {
+				var body = JSON.parse(body);
+				var options = {
+					url: 'https://localhost:3000/restricted',
+					headers: {
+						'referencetoken': body.referenceToken
+					}
+				}
+				request.get(options, (err, response, body) => {
+					body = JSON.parse(body);
+					expect(response.statusCode).toBe(200);
+					expect(body.identityToken).toBeDefined();
+
+					var decoded = jwt.verify(body.identityToken, Config.AuthServer.secret);
+					expect(decoded.id).toBe(1);
+					expect(decoded.username).toBe('jsmith');
+					done();
+				});
+				
+		});
+		
+	});
+
+	it('will require all auth on all routes if default is set to true', (done) => {
+		server.register({ 
+			register: AuthClient,
+			options: {
+				defaultOn: true
+			} 
+		}, (err) => {
+			
+			server.route({
+				method: 'GET',
+				path: '/path1',
+				handler: function(request, reply) {
+					reply({ success: true })
+				}
+			});
+
+			server.route({
+				method: 'GET',
+				path: '/path2',
+				handler: function(request, reply) {
+					reply({ success: true })
+				}
 			});
 		});
+
+		server.start((err) => {
+			expect(err).not.toBeDefined();
+		});
+
+		request.get('http://localhost:3000/path1', (err, response, body) => {
+			expect(response.statusCode).toBe(401);
+			
+			request.get('http://localhost:3000/path2', (err, response, body) => {
+				expect(response.statusCode).toBe(401);
+				done();
+			})
+		})
 	});
 });
 
